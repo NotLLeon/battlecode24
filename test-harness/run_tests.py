e2de21ff2bbe434c92f396c53931afe3b497658a
carloshuang <thebacondragon@gmail.com>
2024-01-10 20:42:18 -0500

    finished getting the output of running games

--- test-harness/run_tests.py
+++ test-harness/run_tests.py
@@ -2,16 +2,16 @@ import os
 import subprocess
 import multiprocessing
 import re
-from itertools import combinations
+from collections import defaultdict
 
 def get_winner_info(input_string):
     match = re.search(r'(\w+) \(A\) wins \(round (\d+)\)|(\w+) \(B\) wins \(round (\d+)\)', input_string)
     if not match:
         return None
     if match.group(1):
-        return {'player': match.group(1), 'winner': 'A', 'round': int(match.group(2))}
+        return {'winningBot': match.group(1), 'winner': 'A', 'round': int(match.group(2))}
     elif match.group(3):
-        return {'player': match.group(3), 'winner': 'B', 'round': int(match.group(4))}
+        return {'winningBot': match.group(3), 'winner': 'B', 'round': int(match.group(4))}
 
 
 def getGameInfo(logStr: str):
@@ -27,7 +27,7 @@ def getGameInfo(logStr: str):
 
 # Expected arguments: (gameMap, teamA, teamB)
 def runGames(args: tuple) -> dict:
-    gameMap, teamA, teamB = args
+    gameMap, teamA, teamB, currBotLabel = args
 
     command = ["./gradlew", "run", f"-Pmaps={gameMap}", f"-PteamA={teamA}",
                f"-PteamB={teamB}"]
@@ -35,23 +35,53 @@ def runGames(args: tuple) -> dict:
     process = subprocess.Popen(command, stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True, cwd=os.pardir)
     output, error = process.communicate()
     gameResInfo = getGameInfo(output)
+
     gameResInfo['fullOutput'] = output
+    gameResInfo['map'] = gameMap
+    gameResInfo['teamA'] = teamA
+    gameResInfo['teamB'] = teamB
+    gameResInfo['currBotLabel'] = currBotLabel
     if error:
         print(f"error is: {error}")
         gameResInfo['error'] = error
     return gameResInfo
 
-def generateStringPairs(botsList):
-    pairsList = list(combinations)
-def runGamesMain(mapsList, botsList):
+def generateGameConfigs(currBot, otherBotsList, mapsList):
+    # Returns a triple of the currBot against every other bot, on every map. Also stores which one is the 'current bot'
+    # returns (currMap, botA, botB, currBotLabel)
+    matchupsList = [(currBot, otherBot, 'A') for otherBot in otherBotsList]
+    matchupsListRevOrder = [(otherBot, currBot, 'B') for otherBot in otherBotsList]
+    matchupsList.extend(matchupsListRevOrder)
+
+    retList = []
+    for currMap in mapsList:
+        for botA, botB, currBotLabel in matchupsList:
+            retList.append((currMap, botA, botB, currBotLabel))
+    return retList
+
+def runGamesMain(currBot, otherBotsList, mapsList):
+    gamesConfigsToRun = generateGameConfigs(currBot, otherBotsList, mapsList)
+
+    with multiprocessing.Pool() as pool:
+        results = pool.map(runGames, gamesConfigsToRun)
+
+    retDict = defaultdict(dict)
+    for resultDict in results:
+        currMap = resultDict['map']
+        currBotLabel = resultDict['currBotLabel']
+        retDict[currMap][f"currBotAsPlayer{currBotLabel}"] = resultDict
+    return retDict
+
+
+
 
 
 
 if __name__ == '__main__':
-    argsList = [('DefaultSmall', 'examplefuncsplayer', 'examplefuncsplayer')]
-    with multiprocessing.Pool() as pool:
-        results = pool.map(runGames, argsList)
+    #TODO error with running multiple gradle jobs in parallel
+    retDict = runGamesMain('examplefuncsplayer',['examplefuncsplayer'], ['DefaultSmall'])
+    print(retDict)
 
-    print(results)
+    # print(results)
 
 
