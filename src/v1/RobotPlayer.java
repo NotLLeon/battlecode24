5a18242f8fa00277b6a96c1e4eab5ca3d65b4d08
Daniel Li <daniel.lisq12@gmail.com>
2024-01-12 17:52:39 -0500

    lock signal bot spawns

--- src/v1/RobotPlayer.java
+++ src/v1/RobotPlayer.java
@@ -31,10 +31,14 @@ public strictfp class RobotPlayer {
                 curRound = rc.getRoundNum();
                 // try to spawn, if spawned on flag, become signal bot
                 if (!rc.isSpawned()) {
-                    MapLocation[] spawnLocs = rc.getAllySpawnLocations();
-                    for (int i = 0; i < 100; i++) {
-                        MapLocation randomLoc = spawnLocs[rng.nextInt(spawnLocs.length)];
-                        if (rc.canSpawn(randomLoc)) rc.spawn(randomLoc);
+                    if (spawnLoc != null) {
+                        if (rc.canSpawn(spawnLoc)) rc.spawn(spawnLoc);
+                    } else {
+                        MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+                        for (int i = 0; i < 100; i++) {
+                            MapLocation randomLoc = spawnLocs[rng.nextInt(spawnLocs.length)];
+                            if (rc.canSpawn(randomLoc)) rc.spawn(randomLoc);
+                        }
                     }
                 } if (turnCount < GameConstants.SETUP_ROUNDS) {
                     // we are in setup phase
