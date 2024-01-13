dfe7582e4b94915fcc4ac3c13e32b6d48b7114a1
Leon Liu <leonyi.liu@gmail.com>
2024-01-13 13:18:42 -0500

    update rush target selectino

--- src/v1/MainPhase.java
+++ src/v1/MainPhase.java
@@ -5,14 +5,43 @@ import static v1.Random.*;
 
 import battlecode.common.*;
 
+import java.util.function.Function;
+
 // MAIN PHASE STRATEGY HERE (TENTATIVE)
 public class MainPhase extends Robot {
 
+    private static final int[] FLAG_INDS = {0, 1, 2};
+    private static final int TARGET_ROUND_INTERVAL = 100; // modify based on map size?
     private static MapLocation[] friendlySpawnLocs = rc.getAllySpawnLocations();
     private static FlagInfo pickedUpFlag = null;
 
     private static void onBroadcast() throws GameActionException {
         FlagRecorder.setApproxFlagLocs();
+    }
+
+    @FunctionalInterface
+    public interface CheckedFunction<T, R> {
+        R apply (T t) throws GameActionException;
+    }
+
+    private static Function<Integer, Boolean> lambdaExceptionWrapper(CheckedFunction<Integer, Boolean> fn) {
+        return i -> {
+            try { return fn.apply(i); }
+            catch(GameActionException e) { return false; }
+        };
+    }
+
+    private static int[] filterFlagInds(CheckedFunction<Integer, Boolean> fn) {
+        int numRemaining = 0;
+        Function<Integer, Boolean> sfn = lambdaExceptionWrapper(fn);
+
+        // idk if this is the best way to do this
+        for (int ind : FLAG_INDS) if (sfn.apply(ind)) ++numRemaining;
+
+        int[] filtered = new int[numRemaining];
+        int i = 0;
+        for (int ind : FLAG_INDS) if (sfn.apply(ind)) filtered[i++] = ind;
+        return filtered;
     }
 
     public static void run() throws GameActionException {
@@ -29,34 +58,28 @@ public class MainPhase extends Robot {
                 FlagRecorder.setCaptured(pickedUpFlag.getID());
                 pickedUpFlag = null;
             }
-            return;
-        }
-        // just rush the first non pickedup flag
-        // if all are pickedup (possibly dropped and respawned), go to non captured flags
-        MapLocation flagLoc = null;
-        for (int i = 0; i < GameConstants.NUMBER_FLAGS; ++i) {
-            if (!FlagRecorder.isPickedUp(i)) {
-                flagLoc = FlagRecorder.getFlagLoc(i);
-                break;
-            }
-            if (!FlagRecorder.isCaptured(i)) {
-                flagLoc = FlagRecorder.getFlagLoc(i);
-            }
-        }
+        } else {
+            // prioritize flags that have not been picked up
+            // if all have been picked up, visit noncaptured flags
+            // switch targets every TARGET_ROUND_INTERVAL rounds
+            int[] rushFlagInds = filterFlagInds((i) -> !FlagRecorder.isPickedUp(i));
+            if (rushFlagInds.length == 0) rushFlagInds = filterFlagInds((i) -> !FlagRecorder.isCaptured(i));
+            if (rushFlagInds.length == 0) rushFlagInds = FLAG_INDS;
 
-        if (flagLoc == null) return;
+            int rushInd = rushFlagInds[(rc.getRoundNum() / TARGET_ROUND_INTERVAL) % rushFlagInds.length];
+            MapLocation rushLoc = FlagRecorder.getFlagLoc(rushInd);
 
-        // TODO: explore within some radius
-        if (rc.getLocation().isAdjacentTo(flagLoc)) Explore.exploreNewArea();
-        else {
-            moveToAdjacent(flagLoc);
+            // TODO: explore within some radius if we are close to approx loc of flag
+            if (rc.getLocation().isAdjacentTo(rushLoc) && !FlagRecorder.isExactLoc(rushInd)) {
+                Explore.exploreNewArea();
+            } else moveToAdjacent(rushLoc);
+
         }
 
         FlagInfo[] visibleEnemyFlags = rc.senseNearbyFlags(-1, rc.getTeam().opponent());
         for (FlagInfo flag : visibleEnemyFlags) {
             if (!flag.isPickedUp()) FlagRecorder.foundFlag(flag);
         }
-
         // Rarely attempt placing traps behind the robot.
         MapLocation prevLoc = rc.getLocation().subtract(nextDir());
         if (rc.canBuild(TrapType.EXPLOSIVE, prevLoc) && nextInt(2) == 0)
