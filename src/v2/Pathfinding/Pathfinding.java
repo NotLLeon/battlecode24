9c553e514c1124420f68884da80a7ae468fae580
Leon Liu <leonyi.liu@gmail.com>
2024-01-14 10:55:13 -0500

    nit

--- src/v2/Pathfinding/Pathfinding.java
+++ src/v2/Pathfinding/Pathfinding.java
@@ -9,6 +9,10 @@ public class Pathfinding {
     // 1 out of every FILLERS_RATIO units are allowed to fill
     private static final int FILLERS_RATIO = 5;
 
+    private static boolean shouldFill() {
+        return (rc.getID() % FILLERS_RATIO == 0) && (rc.getCrumbs() >= GameConstants.FILL_COST);
+    }
+
     public static void moveTo(MapLocation dest, boolean adj, int radius) throws GameActionException {
         MapLocation curLoc = rc.getLocation();
         if(!rc.isMovementReady()
@@ -21,8 +25,7 @@ public class Pathfinding {
         // use BFS when possible, otherwise use BugNav until the obstacle is cleared
         Direction dir = Direction.CENTER;
         if(!BugNav.isTracingObstacle()) {
-            boolean fillAllowed = (rc.getID() % FILLERS_RATIO == 0) && (rc.getCrumbs() >= GameConstants.FILL_COST);
-            dir = BFS.getDir(dest, fillAllowed);
+            dir = BFS.getDir(dest, shouldFill());
             MapLocation nextLoc = curLoc.add(dir);
             if (rc.canFill(nextLoc)) rc.fill(nextLoc);
         }
