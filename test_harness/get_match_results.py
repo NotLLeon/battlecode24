46e8bb7a34344cc34dee37180716f76520157159
carloshuang <thebacondragon@gmail.com>
2024-01-16 00:25:40 -0500

    starting to implment getting the match results

---
+++ test_harness/get_match_results.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from enum import Enum
+
+class PlayerIndex(Enum):
+    REQUESTER_FIRST = 0 # This is teamA
+    REQUESTER_LAST = 1  # This is teamB
+
+
+def getDateTimeToNearestSecond(dateTimeStrFull):
+    parsed_datetime = datetime.fromisoformat(dateTimeStrFull)
+
+    # Round to the nearest second, and this rounds down!
+    rounded_datetime = parsed_datetime.replace(microsecond=0)
+
+    rounded_datetime_str = rounded_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+    return rounded_datetime_str
+
+
+# Datetime string
+datetime_str_input = '2024-01-14T03:11:37.158006-05:00'
+print(getDateTimeToNearestSecond(datetime_str_input))
