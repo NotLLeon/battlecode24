16531d3736732e66933b3b624573b6e8d7db74ca
Leon Liu <leonyi.liu@gmail.com>
2024-01-18 19:00:20 -0500

    update script

--- test_harness/constants.py
+++ test_harness/constants.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from file_operations import getJWTToken, getCurrentGameVersion
 
-DEFAULT_MAPS = ["DefaultMedium", "DefaultSmall", "DefaultHuge", "DefaultLarge"]
+DEFAULT_MAPS = ["DefaultMedium", "AceOfSpades", "Ambush", "Canals", "CH3353C4K3F4CT0RY", "Duck", "Fountain", "Hockey", "Rivers", "Soccer"]
 
 BATTLECODE_URL = "https://api.battlecode.org/api/compete/bc24/request/"
 HEADERS = {"authority": "api.battlecode.org",
