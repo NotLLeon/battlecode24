bcb28f8f687b4e0122bb0ecfa8a66be7a296d672
thermal-ice <thebacondragon@gmail.com>
2024-01-21 18:34:19 -0500

    Support multiple people running queueing script at once (#44)

--- test_harness/get_queued_results.py
+++ test_harness/get_queued_results.py
@@ -8,26 +8,18 @@ from tabulate import tabulate
 import os
 
 NUM_RESULTS_PER_PAGE = 10
-EXTRA_PAGE_LOADED = 1  #Used in case other ppl queue some games in the meanwhile
+EXTRA_PAGES_LOADED = 2  #Used in case other ppl queue some games in the meanwhile
 BASE_URL = 'https://api.battlecode.org/api/compete/bc24/match/scrimmage/?team_id=766'
 class GameStatus(Enum):
     FINISHED = 'OK!'
-
-
-def getDateTimeToNearestSecond(dateTimeStrFull):
-    parsed_datetime = datetime.fromisoformat(dateTimeStrFull)
-
-
-    rounded_datetime = parsed_datetime.replace(second= 0, microsecond=0)
-
-    rounded_datetime_str = rounded_datetime.strftime('%Y-%m-%dT%H:%M')
-    return rounded_datetime_str
+    QUEUED = 'QUE'
+    RUNNING = 'RUN'
 
 
 
 def getAllResults(numGames):
     retList = []
-    numIters = math.ceil(numGames / NUM_RESULTS_PER_PAGE) + EXTRA_PAGE_LOADED
+    numIters = math.ceil(numGames / NUM_RESULTS_PER_PAGE) + EXTRA_PAGES_LOADED
 
     for i in range(numIters):
         pageNum = i + 1
@@ -37,20 +29,70 @@ def getAllResults(numGames):
 
     return retList
 
+
+def getGamesStatusResults(startingPageNum, actualToRequestID):
+    gamesMatchedHM = {} # maps actual ID to info about the game (i.e the whole response body for that game)
+
+    pageNum = startingPageNum
+    # While not all games have been matched yet
+    while len(gamesMatchedHM) < len(actualToRequestID):
+        currURL = BASE_URL + f"&page={pageNum}"
+        resultsList = requests.get(currURL, headers=HEADERS).json()['results']
+
+        for currResult in resultsList:
+            if currResult['id'] in actualToRequestID:
+                gamesMatchedHM[currResult['id']] = currResult
+
+        pageNum += 1
+
+    return gamesMatchedHM
+
+
+
+
+# Page num should start at 1
+def getStartingPageNum(gameID, prevStartingPageNum):
+
+    pageNum = prevStartingPageNum
+    while True:
+
+        currURL = BASE_URL + f"&page={pageNum}"
+        currPageAsJSON = requests.get(currURL, headers=HEADERS).json()
+        for result in currPageAsJSON['results']:
+            if result['id'] == gameID:
+                return pageNum
+
+        pageNum += 1
+
+
+
+
+def isCreationTimeMatching(dateTime1Str, dateTime2Str):
+    dateTime1, dateTime2 = datetime.fromisoformat(dateTime1Str), datetime.fromisoformat(dateTime2Str)
+    timeDiff = abs(dateTime1 - dateTime2)
+
+    return timeDiff.total_seconds() < ACCEPTED_SECONDS_DELTA
+
 def gamesAreMatched(queuedGameResponse, currentGameStatus):
-    queuedGameDateTime, currentGameStatusDateTime = getDateTimeToNearestSecond(queuedGameResponse['creationDateTime']), getDateTimeToNearestSecond(currentGameStatus['created'])
-    if queuedGameDateTime != currentGameStatusDateTime:
+    if not isCreationTimeMatching(queuedGameResponse['creationDateTime'], currentGameStatus['created']):
         return False
 
-    oppBotExpectedIndex = 1 if queuedGameResponse['side'] == PlayerOrder.REQUESTER_FIRST else 0
-    expectedOppBotInfo = currentGameStatus['participants'][oppBotExpectedIndex]
+    # oppBotExpectedIndex = 1 if queuedGameResponse['side'] == PlayerOrder.REQUESTER_FIRST else 0 # This has issue, since we need player_index, not just position in list
 
+    team1Info, team2Info = currentGameStatus['participants']
+    oppBotTeamInfo = team1Info if team1Info['team'] != OUR_TEAM_ID else team2Info
+    if oppBotTeamInfo['teamname'] != queuedGameResponse['oppTeamName']:
+        return False
 
-    return expectedOppBotInfo['teamname'] == queuedGameResponse['oppTeamName']
+    expectedOppPlayerIndex = 1 if queuedGameResponse['side'] == PlayerOrder.REQUESTER_FIRST else 0
+    return oppBotTeamInfo['player_index'] == expectedOppPlayerIndex
+
 
 
 def setGameResultString(gameProgInfo, currentGameStatus, oppBotInfo):
     oppBotName = oppBotInfo['teamname']
+    if oppBotName == "It's A Trap? ":
+        print('got here!')
     oppBotPlayerIndex = oppBotInfo['player_index'] # either 0 or 1
     gameID = currentGameStatus['id']
 
@@ -68,12 +110,12 @@ def setGameResultString(gameProgInfo, currentGameStatus, oppBotInfo):
 
         gameProgInfo[oppBotName][oppBotPlayerIndex] = f"{victoryStatus}  {numGamesWon}-{numGamesLost} {gameID}"
 
-# Maps [oppBot][side] = ResultString
+# Where gameProgInfo: [oppBot][sideOfOpponent] = ResultString
 def displayGames(gameProgInfo):
     dataMatrix = []
     for oppBot in gameProgInfo:
-        requesterFirstGameStr = gameProgInfo[oppBot][0]
-        requesterLastGameStr = gameProgInfo[oppBot][1]
+        requesterFirstGameStr = gameProgInfo[oppBot][1]
+        requesterLastGameStr = gameProgInfo[oppBot][0]
 
         rowStr = [oppBot, requesterFirstGameStr, requesterLastGameStr]
         dataMatrix.append(rowStr)
@@ -81,51 +123,96 @@ def displayGames(gameProgInfo):
     print(tabulate(dataMatrix, headers= ['Opponent bots', 'Requester First', 'Requester Last'], tablefmt="grid"))
 
 
+def setAndDisplayGameFromHM(currAllGamesStatusHM, gameProgInfo):
+    numGamesCompleted = 0
+    for currGameStatus in currAllGamesStatusHM.values():
+        if currGameStatus['status'] == GameStatus.FINISHED.value:
+            numGamesCompleted += 1
+        oppTeamInfo= currGameStatus['participants'][1] if currGameStatus['participants'][0]['team'] == OUR_TEAM_ID else currGameStatus['participants'][0]
+        setGameResultString(gameProgInfo, currGameStatus, oppTeamInfo)
 
-# Matches the games requested with the games in the queue
-# matches based on opposingBotName, side, and creation date
-def matchGames(requestedGamesResponse, gameReplaysDict):
+    displayGames(gameProgInfo)
+
+    return numGamesCompleted
+
+
+def matchGamesFirstTime(requestedGamesResponse, gameProgInfo):
+    def addToIDMaps(requestID, actualID):
+        if requestID in requestToActualID or actualID in actualToRequestID:
+            print("ERROR: Setting the ID map should only happen once!")
+        requestToActualID[requestID] = actualID
+        actualToRequestID[actualID] = requestID
+
     numGamesPlayed = len(requestedGamesResponse) * 2 # List of pairs
     allGamesInfo = getAllResults(numGamesPlayed) # Can parallelize this if needed
 
 
+    # Both these dicts only contain IDs of our games, not any extra!
+    requestToActualID = {} # Maps the request's gameID to the actual ID
+    actualToRequestID = {} # Maps the actual ID to request's ID
+
+
+
+    while len(requestToActualID) < numGamesPlayed:
+
+        # These are the games that we've requested, and it's info
+        for game1Info, game2Info in requestedGamesResponse:
+
+            # These are the actual games information
+            for currGameResult in allGamesInfo:
+
+                oppTeamInfo = currGameResult['participants'][0] if currGameResult['participants'][0]['team'] != OUR_TEAM_ID else currGameResult['participants'][1]
+                # Match games based on opponent name, player_index, creation time
+                if gamesAreMatched(game1Info, currGameResult):
+
+                    setGameResultString(gameProgInfo,currGameResult, oppTeamInfo)
+                    addToIDMaps(game1Info['id'], currGameResult['id'])
+
+                elif gamesAreMatched(game2Info, currGameResult):
+                    setGameResultString(gameProgInfo,currGameResult, oppTeamInfo)
+                    addToIDMaps(game2Info['id'], currGameResult['id'])
+
+
+
+    return requestToActualID, actualToRequestID
+
+def setReplaysDict(gameReplaysDict, currAllGamesStatusHM):
+    for gameID in currAllGamesStatusHM:
+        gameReplaysDict[gameID] = REPLAYS_WATCH_BASE_URL + currAllGamesStatusHM[gameID]['replay_url']
+
+
+def outputMatchGames(requestedGamesResponse, gameReplaysDict):
+
     # Maps [oppBot][side] = ResultString
     gameProgInfo = defaultdict(lambda: [None,None])
 
+    requestToActualID, actualToRequestID = matchGamesFirstTime(requestedGamesResponse,gameProgInfo)
+    lastGameQueued = requestedGamesResponse[-1][1]
+    # Setting my own team name happens here
 
-    completedGamesSet = set() # Is a set of all completed games
+    lastGameActualID = requestToActualID[lastGameQueued['id']]
+    startingPageNum = getStartingPageNum(lastGameActualID,1)
+    #TODO Now need to loop and call getGamesStatusResults(startingPageNum, actualToRequestID) a bunch, until all games have been completed
+
+    numGamesTotal = len(requestToActualID)
+
     numGamesCompleted = 0
-    clearCommand = 'cls' if os.name == 'nt' else 'clear'
-    while numGamesCompleted < numGamesPlayed:
 
-        for game1Info, game2Info in requestedGamesResponse:
-            # Yes this is inefficient and I can change into map, but this doesn't actually affect runtime so whatevs
+    while numGamesCompleted != numGamesTotal:
+        currAllGamesStatusHM = getGamesStatusResults(startingPageNum, actualToRequestID)
+        numGamesCompleted = setAndDisplayGameFromHM(currAllGamesStatusHM, gameProgInfo)
 
-            for currGameResult in allGamesInfo:
-                gameMatched = False
 
-                # Match games based on opponent name, player_index, creation time
-                if gamesAreMatched(game1Info, currGameResult):
-                    setGameResultString(gameProgInfo,currGameResult, currGameResult['participants'][1])
-                    gameMatched = True
+        startingPageNum = getStartingPageNum(lastGameActualID, startingPageNum) # Refreshing the starting page to go from
+        setReplaysDict(gameReplaysDict, currAllGamesStatusHM) # Setting the replays dict stuff
+        print(f'{numGamesCompleted} out of {numGamesTotal} games completed\n')
 
-                elif gamesAreMatched(game2Info, currGameResult):
-                    setGameResultString(gameProgInfo,currGameResult, currGameResult['participants'][0])
-                    gameMatched = True
+        time.sleep(MATCH_RESULTS_REFRESH_DELAY)
 
-                # Check if match is competed, and save the replay url (only once though!)
-                if currGameResult['status'] == 'OK!' and gameMatched and currGameResult['id'] not in completedGamesSet:
-                    numGamesCompleted += 1
-                    completedGamesSet.add(currGameResult['id'])
-                    gameReplaysDict[currGameResult['id']] = REPLAYS_WATCH_BASE_URL+ currGameResult['replay_url']
 
-        # Clearing the terminal of prev games
-        os.system(clearCommand)
-        displayGames(gameProgInfo)
-        # Wait for 5 seconds in between refreshing for new list of games
-        print(f'{numGamesCompleted} out of {numGamesPlayed} games completed')
-        allGamesInfo = getAllResults(numGamesPlayed)
-        time.sleep(5)
+
+
+
 
 
 
