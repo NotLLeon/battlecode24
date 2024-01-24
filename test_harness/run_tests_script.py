2ac1813bbbedacefed8038f58c0e0699d5c67629
thermal-ice <thebacondragon@gmail.com>
2024-01-24 18:08:24 -0500

    switch teams and maps around in the output display (#57)

--- test_harness/run_tests_script.py
+++ test_harness/run_tests_script.py
@@ -6,23 +6,22 @@ from constants import *
 from file_operations import *
 
 
-DEFAULT_BOTS = ['v0', 'examplefuncsplayer']
-DEFAULT_MAPS = ['DefaultSmall', 'DefaultMedium', 'DefaultLarge', 'DefaultHuge']
 
 def main():
     parser = argparse.ArgumentParser(description='Runs Battlecode games and prints out the results')
 
 
-    # Optional flag arguments
-    parser.add_argument('-b', '--bots', nargs='+', type=str, default=DEFAULT_BOTS, help='Bots to match up against')
+    # Mandatory arg, which bots to play against
+    parser.add_argument('-b', '--bots', nargs='+', type=str, required=True, help='Bots to match up against')
 
+    # Optional args
     mapsGroup = parser.add_mutually_exclusive_group(required=False)
     mapsGroup.add_argument('-m', '--maps', nargs='+', type=str, help='Maps to play on')
     mapsGroup.add_argument('-mf', '--mapsfile', type=str, help='Path to a file containing list of maps to play on')
     mapsGroup.add_argument('-ma', '--mapsall', action='store_true', help='Select all maps to play on, as defined by ALL_MAPS in constant.py')
 
 
-# Mandatory args
+    # Mandatory args
     parser.add_argument('curr_bot', type=str, help='Name of the current bot you want to test')
 
     args = parser.parse_args()
@@ -50,7 +49,7 @@ def main():
     # print(gameAnalyticsInfo)
 
     resDictTransformed = transformGameDataAsNestedMap(gamesResultsList)
-    printTabulateAsGrid(resDictTransformed, mapsList=mapsList)
+    printTabulateAsGrid(resDictTransformed, botsVersusList=oppBots)
 
 
 
